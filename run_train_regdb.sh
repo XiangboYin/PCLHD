e9c5bb6b085eafc2ced2031603188e6c93d64dd9
Xiangbo Yin <92357317+XiangboYin@users.noreply.github.com>
2024-09-30 11:10:18 +0800

    Update run_train_regdb.sh

--- run_train_regdb.sh
+++ run_train_regdb.sh
@@ -1,7 +1,7 @@
 for trial in 1 2 3 4 5 6 7 8 9 10
 do
 CUDA_VISIBLE_DEVICES=0,1 \
-python train_regdb.py -b 128 -a agw -d  regdb_rgb --iters 100 \
+python train_regdb.py -b 128 -a agw -d regdb_rgb -mb CMhybrid --iters 100 \
 --momentum 0.1 --eps 0.3 --num-instances 16 --trial $trial \
 --data-dir "/data/yxb/datasets/ReIDData/RegDB/"
 done
